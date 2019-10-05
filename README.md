7ca5236b3f6c5a4773a962697391a6881dc63e29
Joshua Martinez <joshkmartinez@gmail.com>
2019-10-05 12:06:06 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# flush-dns
+This is a simple little tool at makes it easy for you to flush your DNS cache.
+## Installation
+```npm i flush-dns -g```
+## Usage
+Simply run `flush-dns`. That's it.
---
+++ index.js
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+
+const chalk = require("chalk");
+const clear = require("clear");
+var shell = require("shelljs");
+const os = require("os");
+const ERROR_MSG = "Error: DNS Flush failed.";
+const SUCCESS_MSG = "DNS cache has been flushed.";
+clear();
+console.log("Operating platform: " + os.type() + ".");
+console.log(`The parent process is pid ${process.ppid}`);
+console.log(chalk.blue("Flushing DNS cache... "));
+console.log("You may be asked to enter your password.")
+if (os.type().toLowerCase() == "darwin") {
+  if (
+    shell.exec("sudo dscacheutil -flushcache;sudo killall -HUP mDNSResponder")
+      .code !== 0
+  ) {
+    shell.echo(ERROR_MSG);
+    shell.exit(1);
+    console.log(
+      "This tool is not supported on operating systems lower than OS X 10.10.4"
+    );
+  } else {
+    console.log(chalk.blue(SUCCESS_MSG));
+  }
+} else if (os.type() == "win32") {
+  if (shell.exec("ipconfig /flushdns").code !== 0) {
+    shell.echo(ERROR_MSG);
+    shell.exit(1);
+  } else {
+    console.log(chalk.blue(SUCCESS_MSG));
+  }
+} else if (os.type() == "linux") {
+  if (
+    shell.exec(
+      "sudo /etc/init.d/dns-clean restart;sudo /etc/init.d/networking force-reload"
+    ).code !== 0
+  ) {
+    shell.echo(ERROR_MSG);
+    shell.exit(1);
+  } else {
+    console.log(chalk.blue(SUCCESS_MSG));
+    console.log("Did you know Ubuntu doesn’t cache DNS entries by default?");
+    console.log(
+      "If you have installed a DNS service here are some ways you can clear its cache."
+    );
+    console.log("nsdc: sudo /etc/init.d/nscd restart");
+    console.log("dnsmasq: sudo /etc/init.d/dnsmasq restart");
+    console.log("BIND: sudo /etc/init.d/named restart");
+  }
+} else {
+  console.log("Your operating system is not supported.");
+}
